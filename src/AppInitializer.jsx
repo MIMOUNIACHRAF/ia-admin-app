a587a67ec1afc26a58efe84843e00c2b452a9a43
MIMOUNIACHRAF <achraf.mimouni99@gmail.com>
2025-09-03 20:10:50 +0100

    version 1.1.7 R inverser logique AppInitializer + authService

--- src/AppInitializer.jsx
+++ src/AppInitializer.jsx
@@ -34,11 +34,11 @@ export default function AppInitializer({ children }) {
       // Refresh token absent → vider tout et rediriger
       if (!refreshExists) {
         console.log("Refresh token absent → vider tout et rediriger");
-        authService.clearAccessToken();
-        authService.clearRefreshToken();
-        localStorage.clear();
-        dispatch(logout());
-        navigate("/login", { replace: true });
+        // authService.clearAccessToken();
+        // authService.clearRefreshToken();
+        // localStorage.clear();
+        // dispatch(logout());
+        // navigate("/login", { replace: true });
         return;
       }
 
