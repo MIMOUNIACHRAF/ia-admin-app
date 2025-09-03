283e3bd0b34aed4983bd806180528acf77a99ccc
MIMOUNIACHRAF <achraf.mimouni99@gmail.com>
2025-09-03 18:23:40 +0100

    version 1.1.7 R inverser logique AppInitializer + authServicese

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import AppRoutes from "./routes/routes";
 import AppInitializer from "./AppInitializer";
-
+import store from "./store"; // vérifie bien ton chemin vers le store Redux
+import store from "./store";
 function App() {
   return (
-    <BrowserRouter>
-      <AppInitializer>
-        <AppRoutes />
-      </AppInitializer>
-    </BrowserRouter>
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <AppInitializer>
+            <AppRoutes />
+          </AppInitializer>
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
   );
 }
 
